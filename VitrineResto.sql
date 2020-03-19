33676e2ddf8bd4f62bb04eb62beac1c865f6cb02
Arnaud <arnaud.clarat@live.fr>
2020-03-19 19:43:32 +0100

    Changement DB
    prix.plat => prix.plat_prix (57)

--- VitrineResto.sql
+++ VitrineResto.sql
@@ -44,7 +44,7 @@ CREATE TABLE IF NOT EXISTS `t_plats` (
     `id_plat` int(11) NOT NULL AUTO_INCREMENT,
     `nom_plat` varchar(250) NOT NULL,
     `details_plat` varchar(2048) NOT NULL,
-    `prix` decimal(4,2),
+    `prix_plat` decimal(4,2),
     `categ_plat` int(11) NOT NULL,
     PRIMARY KEY (`id_plat`),
     FOREIGN KEY (`categ_plat`) REFERENCES `t_categ` (`id_categ`)
@@ -54,7 +54,7 @@ CREATE TABLE IF NOT EXISTS `t_plats` (
 -- Déchargment de la table `t_plats`
 --
 
-INSERT INTO `t_plats` (`nom_plat`, `details_plat`, `prix`, `categ_plat`) VALUES
+INSERT INTO `t_plats` (`nom_plat`, `details_plat`, `prix_plat`, `categ_plat`) VALUES
 ('Burger', 'Burger 100% pur boeuf accompagné de frites et salade, sauce au choix', 10, 1),
 ('Steak', 'Steak de 150g accompagné de frites et salade, sauce au choix', 11.5, 1),
 ('Filet de pangasius', 'accompagné de frites et salade, sauce au choix', 13, 2),
